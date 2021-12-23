2fbb1eb6afb1c05fa23086f93475b2b8efeb71fd
Carlos <czelayabaca@gmail.com>
2021-12-23 12:15:04 -0600

    Aqui esta ya con los estilos e iconos la barra de menu

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { HomeScreen, RapisScreen, FavoritesScreen, AjustesScreen } from './Screens/index';
+import { FontAwesome5, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { HomeScreen, RapisScreen, FavoritesScreen , AjustesScreen } from './Screens/index';
+import { StyleSheet, View } from 'react-native';
 
 const AppNavigator = createBottomTabNavigator({
   Home: {
@@ -16,6 +18,63 @@ const AppNavigator = createBottomTabNavigator({
   Ajustes: {
     screen: AjustesScreen,
   }
-}, { initialRouteName: 'Home' })
+}, 
+{ 
+  initialRouteName: 'Home',
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarOptions: { // El objeto que utilizamos par hacer modificaciones a nuestra barra de menú
+      style: { //Estilo a la barra de navegacion
+        backgroundColor: '#667eea',
+        height: 55,
+        borderTopLeftRadius: 20,
+        // backgroundColor: '#764ba2'
+      },
+      labelStyle: { //Estilo a la fuente de nuestros menus
+        fontSize: 13,
+        fontWeight: '300',
+      },
+      activeTintColor: 'blue', // Color del texto de la pantalla en ejecucion
+      inactiveTintColor: '#000', //Color de los textos de las pantallas inactivas
+  },
+  tabBarIcon: ({ focused }) => {
+    const { routeName } = navigation.state;
+    let iconName
+    let iconNameIonicon
+    let iconNameMaterial
+
+      if(routeName === 'Home'){
+        iconNameIonicon = `${focused ? 'home-outline' : 'ios-home'}`;
+      }
+
+      if(routeName === 'Rapis'){
+        iconName = 'motorcycle'
+      }
+
+      if(routeName === 'Favoritos'){
+        iconNameMaterial = `${focused ? 'favorite-outline' : 'favorite'}`;
+      }
+
+      if(routeName === 'Ajustes'){
+        iconNameIonicon = `${focused ? 'settings-outline' : 'settings-sharp'}`
+      }
+      
+        return (
+          <View style={ styles.centrar }>
+            <MaterialIcons name={iconNameMaterial} size={25} />
+            <Ionicons name={iconNameIonicon} size={25} />
+            <FontAwesome5 name={iconName} size={25} />
+          </View>
+        )
+
+  }
+})
+}
+)
 
 export default createAppContainer(AppNavigator);
+
+const styles = StyleSheet.create({
+  centrar: {
+    flexDirection: 'row',
+  }
+})
--- Screens/RapisScreen.js
+++ Screens/RapisScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View, Text } from 'react-native';
 
+
 const RapisScreen = () => {
     return (
       <View style={styles.container}>
